51c5c282e4de81901445b3b442bc302cc5b2428c
fogelo <fogelo@yandex.ru>
2022-12-19 16:17:11 +0300

    рефакторинг: замена условной конструкции полиморфизмом

--- createStatementData.js
+++ createStatementData.js
@@ -8,13 +8,24 @@ export default function createStatementData(invoice) {
 }
 
 function enrichPerformance(aPerformance) {
-    const calculator = new PerformanceCalculator(aPerformance, playFor(aPerformance))
+    const calculator = createPerformanceCalculator(aPerformance, playFor(aPerformance))
     const result = Object.assign({}, aPerformance);
     result.play = calculator.play
     result.amount = calculator.amount
     result.volumeCredits = calculator.volumeCredits
 
     return result;
+}
+
+function createPerformanceCalculator(aPerformance, aPlay) {
+    switch (aPlay.type) {
+        case "tragedy":
+            return new TragedyCalculator(aPerformance, aPlay)
+        case "comedy":
+            return new ComedyCalculator(aPerformance, aPlay)
+        default:
+            throw new Error(`unknown type: ${aPlay.type}`)
+    }
 }
 
 class PerformanceCalculator {
@@ -24,34 +35,39 @@ class PerformanceCalculator {
     }
 
     get amount() {
-        let result = 0
-        switch (this.play.type) {
-            case "tragedy":
-                result = 40000;
-                if (this.performance.audience > 30) {
-                    result += 1000 * (this.performance.audience - 30);
-                }
-                break;
-            case "comedy":
-                result = 30000;
-                if (this.performance.audience > 20) {
-                    result += 10000 + 500 * (this.performance.audience - 20);
-                }
-                result += 300 * this.performance.audience;
-                break;
-            default:
-                throw new Error(`unknown type: ${this.play.type}`);
+        throw new Error("subclass responsibility")
+    }
+
+    get volumeCredits() {
+        return Math.max(this.performance.audience - 30, 0);
+    }
+}
+
+class TragedyCalculator extends PerformanceCalculator {
+    get amount() {
+        let result = 40000;
+        if (this.performance.audience > 30) {
+            result += 1000 * (this.performance.audience - 30);
         }
         return result
     }
-    get volumeCredits() {
-        let result = 0;
-        result += Math.max(this.performance.audience - 30, 0);
-        if ("comedy" === this.performance.play.type) result += Math.floor(
-            this.performance.audience / 5);
+}
+
+class ComedyCalculator extends PerformanceCalculator {
+    get amount() {
+        let result = 30000;
+        if (this.performance.audience > 20) {
+            result += 10000 + 500 * (this.performance.audience - 20);
+        }
+        result += 300 * this.performance.audience;
         return result
     }
+    //через super можно обращаться к методам родительского класса в подклассах
+    get volumeCredits() {
+        return super.volumeCredits + Math.floor(this.performance.audience / 5)
+    }
 }
+
 
 function playFor(aPerformance) {
     return plays[aPerformance.playlD]
